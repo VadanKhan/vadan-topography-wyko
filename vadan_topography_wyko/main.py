19c20d7bf3613d8d6bba383276b2d72169dab88a
{Vadan Khan} <{vadankhan1@gmail.com}>
2024-10-29 15:43:13 +0000

    updated user inputs

--- vadan_topography_wyko/main.py
+++ vadan_topography_wyko/main.py
@@ -17,7 +17,7 @@ This code will:
 User input required:
     1. input and output path.
     2. Samples information.
-    3. Plot colors.
+    3. Plot COLORS.
     4. specify parameters for edge detection, only change it when needed.
 
 Output files by this code:
@@ -34,12 +34,12 @@ Output files by this code:
        further process.
 
 Note: the vector length of waferIDs, cubeIDs, designinfos, machinenames,
-colors should be the same, matching how many stamp samples you want to
+COLORS should be the same, matching how many stamp samples you want to
 process together. And the sequence of information in vectors should match
 real samples. This info will be auto-applied to plots/table/file names.
 
 Multiple Stamp samples with the same array size can be processed at once.
-Each stamp sample has its own folder under 'inputPath', with the name format
+Each stamp sample has its own folder under 'INPUTPATH', with the name format
 of 'waferID_CUBE_cubeID', for example, 'NVIIH_CUBE_161'. Inside the folder,
 individual opd files are autosaved by Wyko, with the naming format of
 'Row_#_Col_#_.fc.opd' by STST Wyko 582, or 'row#column#.fc.opd' by NRM
@@ -79,7 +79,7 @@ from filters import crown_average_filter
 # Close all plots, clear data in workspace and records in Command Window.
 plt.close('all')
 # Clear all variables (not directly applicable in Python, but resetting relevant variables)
-datasets = []
+DATASETS = []
 
 
 # ---------------------------------------------------------------------------- #
@@ -88,89 +88,94 @@ datasets = []
 #region USER INPUTS
 
 # ---------------- Input and Output Paths + Analysis Plot Names -------------- #
-inputPath = 'C:\\Users\\762093\\Documents\\WYKO_DATA'
-outputPath = 'C:\\Users\\762093\\Documents\\WYKO_DATA\\OUTPUTS\\output_debug'
+INPUTPATH = 'C:\\Users\\762093\\Documents\\WYKO_DATA'
+OUTPUTPATH = 'C:\\Users\\762093\\Documents\\WYKO_DATA\\OUTPUTS\\output_debug'
 
-campaign_name = 'nxhpp_comparisons'
+CAMPAIGN_NAME = 'nxhpp_comparisons'
 
 
-# ---------------------------- Datasets to Analyse --------------------------- #
-datasets = [
+# ---------------------------- DATASETS to Analyse --------------------------- #
+DATASETS = [
     'NEHZX_CUBE_161',
     'NEHZX_CUBE_167',
-    # Add more datasets as needed
+    # Add more DATASETS as needed
 ]
-numData = len(datasets)
+NUMDATA = len(DATASETS)
 
 
-# ------- input the number of rows and columns in measured laser array. ------- #
-rows = 1
-cols = 1
-rowrange = range(1, rows + 1)
-colrange = range(1, cols + 1)
-laserIDrange = range(1, len(rowrange) * len(colrange) + 1)
+# ------- input the number of ROWS and columns in measured laser array. ------- #
+ROWS = 1
+COLS = 1
+ROWRANGE = range(1, ROWS + 1)
+COLRANGE = range(1, COLS + 1)
+LASERIDRANGE = range(1, len(ROWRANGE) * len(COLRANGE) + 1)
 
 
 # -------------------------- Colours of Each Dataset ------------------------- #
-colors = [[1, 0.5, 0], [0.5, 0, 0.5], [1, 0.5, 0], [0, 0, 1], [1, 0, 1]]
+COLORS = [[1, 0.5, 0], [0.5, 0, 0.5], [1, 0.5, 0], [0, 0, 1], [1, 0, 1]]
 
 
 # Saved Images Quality (300 for decent runtime)
-imgqual = 300
+IMGQUAL = 300
 
 
 # Contour Plot Z limit (range = 2*zlim, measured in nm)
-zlim = 400
+ZLIM = 400
+
+
+# Define Maps for edge and centre cubes
+EDGE_POINTS = ['166', '167', '147', '148', '185', '128', '177', '157', '158', '138', '139', '120', '205', '195', '90', '81']
+CENTRE_POINTS = ['161', '162', '163', '142', '143', '144', '123', '124', '136']
 
 
 # ------------------------------ Filter Settings ----------------------------- #
-# The "delta_threshold" will give the maximum allowed difference between adjacent 
+# The "DELTA_THRESHOLD" will give the maximum allowed difference between adjacent 
 # heights (in nm). If above this value, the code will filter out this data point
 # as unphysical
 # 
-# The "anomaly_threshold" gives a similar maximum for a separate filter. This filter
+# The "ANOMALY_THRESHOLD" gives a similar maximum for a separate filter. This filter
 # runs an average of 60 points surrounding each point, and if the target is 
 # different above this set threshold, then it is filtered out as unphysical.
-# This should be set higher than the "delta_threshold".
-apply_delta_filter = True
-apply_average_filter = True
+# This should be set higher than the "DELTA_THRESHOLD".
+APPLY_DELTA_FILTER = True
+APPLY_AVERAGE_FILTER = True
 
-delta_threshold = 3  # Adjust this value as needed
-anomaly_threshold = 25  # Adjust this value as needed
-window_size_input = 20 # Adjust this value as needed
+DELTA_THRESHOLD = 3  # Adjust this value as needed
+ANOMALY_THRESHOLD = 25  # Adjust this value as needed
+WINDOW_SIZE_INPUT = 20 # Adjust this value as needed
 
 
 # --------------- Image Detection Parameters for Edge Detection -------------- #
-edgedetect = 3 # parameter for edge detect function. only change when needed.
-RctangleCS_leftedge = [[200, 450], [220, 260]] # window for left edge detect, specify X,Y ranges.
+EDGEDETECT = 3 # parameter for edge detect function. only change when needed.
+LEFTEDGEWINDOW = [[200, 450], [220, 260]] # window for left edge detect, specify X,Y ranges.
 #endregion
 
 
 # ------- Option to group and color label plots based on 'design infos' ------ #
-group_by_design_info = False  # Set to true to group by design infos, false to group by waferID and cubeID
+GROUP_BY_DESIGN_INFO = False  # Set to true to group by design infos, false to group by waferID and cubeID
 
-design_infos = ['S1.7g',]  # Few Files Check
+DESIGN_INFOS = ['S1.7g',]  # Few Files Check
 
-colours_design_organised = [[0, 1, 1], [0.5, 0, 0.5], [1, 0.5, 0], [0.5, 0.5, 0], [1, 0, 1]]
+COLOURS_DESIGN_ORGANISED = [[0, 1, 1], [0.5, 0, 0.5], [1, 0.5, 0], [0.5, 0.5, 0], [1, 0, 1]]
 
 
 # --------------------------- Different Array Sizes -------------------------- #
 #  Set this to true if you want different array sizes within an analysis
 #  batch. The sizes can be set in the row_dynamic and column_dynamic vectors
 # . row_dynamic and column_dynamic ARE NOT USED IF THIS IS SET TO FALSE, ALL
-# THE ARRAY SIZES ARE PRESUMED TO FOLLOW THE INITIAL "rows" and "columns"
+# THE ARRAY SIZES ARE PRESUMED TO FOLLOW THE INITIAL "ROWS" and "columns"
 # setting
-dynamic_arrays = False
+DYNAMIC_ARRAYS = False
 
-row_dynamic = []
+ROW_DYNAMIC = []
 
-column_dynamic = []
+COLUMMN_DYNAMIC = []
 
 
 # ------------------------- Plotting Indexing Option: ROW  ------------------------- #
 # NOTE that the for loop needs to edited (for each row, for each column)
 # for this to be complete
-plot_by_column = False
+PLOT_BY_COLUMN = False
 
 #endregion USER INPUTS
 
@@ -181,18 +186,14 @@ plot_by_column = False
 #region Pre-Processing.
 
 # Create output directory if it doesn't exist
-os.makedirs(outputPath, exist_ok=True)
-
-rowrange = range(1, rows + 1)
-colrange = range(1, cols + 1)
-laserIDrange = range(1, len(rowrange) * len(colrange) + 1)
+os.makedirs(OUTPUTPATH, exist_ok=True)
 
 # Create variables for data storage.
-data_raw = [[None for _ in range(numData)] for _ in range(len(laserIDrange))]  # store all raw data from opd files.
-data_processed = [[None for _ in range(numData)] for _ in range(len(laserIDrange))]  # store all processed data.
-data_crownprofiles = [[None for _ in range(numData)] for _ in range(len(laserIDrange))]  # store all longitudinal profiles.
-data_xcrownprofiles = [[None for _ in range(numData)] for _ in range(len(laserIDrange))]  # store all transverse profiles.
-data_crowns = np.nan * np.zeros((len(laserIDrange) * numData, 3))  # 1 is crown, 2,3 is xcrown.
+data_raw = [[None for _ in range(NUMDATA)] for _ in range(len(LASERIDRANGE))]  # store all raw data from opd files.
+data_processed = [[None for _ in range(NUMDATA)] for _ in range(len(LASERIDRANGE))]  # store all processed data.
+data_crownprofiles = [[None for _ in range(NUMDATA)] for _ in range(len(LASERIDRANGE))]  # store all longitudinal profiles.
+data_xcrownprofiles = [[None for _ in range(NUMDATA)] for _ in range(len(LASERIDRANGE))]  # store all transverse profiles.
+data_crowns = np.nan * np.zeros((len(LASERIDRANGE) * NUMDATA, 3))  # 1 is crown, 2,3 is xcrown.
 #endregion
 
 
@@ -203,12 +204,12 @@ data_crowns = np.nan * np.zeros((len(laserIDrange) * numData, 3))  # 1 is crown,
 # Loop to read and process all opd files
 
 # ITERATE FOR EACH DATASET
-for dataset in datasets:
+for dataset in DATASETS:
     waferID, cubeID = dataset.split('_CUBE_')
     print(f"{waferID} CUBE {cubeID} is in processing.")
     
     # Define the input path for the current cube
-    cubePath = os.path.join(inputPath, dataset)
+    cubePath = os.path.join(INPUTPATH, dataset)
     
     # Detect the format of the .opd file
     test_files = [f for f in os.listdir(cubePath) if f.endswith('.fc.opd')]
@@ -227,8 +228,8 @@ for dataset in datasets:
     #endregion Iterative Loop
     
     # ITERNATE FOR ALL LASERS PER DATASET
-    for rowID in rowrange:
-        for colID in colrange:
+    for rowID in ROWRANGE:
+        for colID in COLRANGE:
             opdfilename = opdfilenameformat.format(rowID, colID)
             filename = os.path.join(cubePath, f"{opdfilename}.fc.opd")
             
@@ -267,11 +268,11 @@ for dataset in datasets:
             #region Image Processing
                 
             # ------------------------------ Edge Detection ------------------------------ #
-            laser_edge, image_raw_positive = edge_detection(image_raw, edgedetect)
+            laser_edge, image_raw_positive = edge_detection(image_raw, EDGEDETECT)
             
             
             # ----------------------------- Laser Orientation ---------------------------- #    
-            leftedge_angle, center_CS = estimate_rotation_and_cs(laser_edge, Resolution, RctangleCS_leftedge, image_raw)
+            leftedge_angle, center_CS = estimate_rotation_and_cs(laser_edge, Resolution, LEFTEDGEWINDOW, image_raw)
             # print(f"Left Edge Angle: {leftedge_angle}")
             # print(f"Center Coordinate System: {center_CS}")
             
@@ -289,18 +290,20 @@ for dataset in datasets:
             #region Profile Extraction
             crown_profile, xcrown_profile = extract_crown_profiles(data_processed, Resolution)
             
-            # Apply filters to the crown and xcrown profiles  
-            partiallyfiltered_crown_profile = crown_delta_filter(crown_profile, delta_threshold)
-            partiallyfiltered_xcrown_profile = crown_delta_filter(xcrown_profile, delta_threshold)
-            filtered_crown_profile = crown_average_filter(partiallyfiltered_crown_profile, window_size=window_size_input, threshold=anomaly_threshold)
-            filtered_xcrown_profile = crown_average_filter(partiallyfiltered_xcrown_profile, window_size=window_size_input, threshold=anomaly_threshold)
+            # Apply filters to the crown and xcrown profiles
+            if APPLY_DELTA_FILTER:
+                crown_profile = crown_delta_filter(crown_profile, DELTA_THRESHOLD)
+                xcrown_profile = crown_delta_filter(xcrown_profile, DELTA_THRESHOLD)
+            if APPLY_AVERAGE_FILTER:
+                crown_profile = crown_average_filter(crown_profile, window_size=WINDOW_SIZE_INPUT, threshold=ANOMALY_THRESHOLD)
+                xcrown_profile = crown_average_filter(xcrown_profile, window_size=WINDOW_SIZE_INPUT, threshold=ANOMALY_THRESHOLD)
 
             # Calculate crown_values and xcrown values
             edgedistance = 0  # pixel numbers
-            crown_value = 0 - 0.5 * (filtered_crown_profile[edgedistance, 1] + filtered_crown_profile[-edgedistance - 1, 1])
+            crown_value = 0 - 0.5 * (crown_profile[edgedistance, 1] + crown_profile[-edgedistance - 1, 1])
             
-            xcrownP_value = 0 - filtered_xcrown_profile[edgedistance, 1]
-            xcrownN_value = 0 - filtered_xcrown_profile[-edgedistance - 1, 1]
+            xcrownP_value = 0 - xcrown_profile[edgedistance, 1]
+            xcrownN_value = 0 - xcrown_profile[-edgedistance - 1, 1]
             
             print(f"YCrown: {crown_value}")
             print(f"XCrownP: {xcrownP_value}")
