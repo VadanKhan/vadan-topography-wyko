623ad80c59de1ace4c8e048452b7002255069c3a
{Vadan Khan} <{vadankhan1@gmail.com}>
2024-11-13 10:42:03 +0000

    statistics saving

--- main-notebook.py
+++ main-notebook.py
@@ -8,20 +8,38 @@ import os
 import traceback
 # from surfalize import Surface
 
-from opdread_package import read_wyko_opd
+# Import the modules
+import vadan_topography_wyko.opd_read as opd_read
+import edge_detect
+import laser_orientation
+import flattening
+import crown_extraction
+import filters
+
+# Import the importlib module
+import importlib
+
+# Reload the modules
+importlib.reload(opd_read)
+importlib.reload(edge_detect)
+importlib.reload(laser_orientation)
+importlib.reload(flattening)
+importlib.reload(crown_extraction)
+importlib.reload(filters)
+
+# Re-import the functions
+from vadan_topography_wyko.opd_read import read_wyko_opd
 from edge_detect import edge_detection
 from laser_orientation import estimate_rotation_and_cs
 from flattening import flatten
 from crown_extraction import extract_crown_profiles
-from filters import crown_delta_filter
-from filters import crown_average_filter
+from filters import crown_delta_filter, crown_average_filter
 
 #endregion
 # Close all plots, clear data in workspace and records in Command Window.
 plt.close('all')
 # Clear all variables (not directly applicable in Python, but resetting relevant variables)
 DATASETS = []
-
 
 # %%
 #                      USER INPUT / CONFIGURATION SECTION                      #
@@ -37,7 +55,9 @@ CAMPAIGN_NAME = 'nxhpp_comparisons'
 
 # ---------------------------- DATASETS to Analyse --------------------------- #
 DATASETS = [
-    'NEHZX_CUBE_161',
+    # 'ASTRO_CUBE_1',
+    'QDHIE_CUBE_161',
+    # 'ASTRO_CUBE_3',
     # Add more DATASETS as needed
 ]
 NUMDATA = len(DATASETS)
@@ -78,15 +98,14 @@ CENTRE_POINTS = ['161', '162', '163', '142', '143', '144', '123', '124', '136']
 APPLY_DELTA_FILTER = True
 APPLY_AVERAGE_FILTER = True
 
-DELTA_THRESHOLD = 3  # Adjust this value as needed
+DELTA_THRESHOLD = 10  # Adjust this value as needed
 ANOMALY_THRESHOLD = 25  # Adjust this value as needed
 WINDOW_SIZE_INPUT = 20 # Adjust this value as needed
 
 
 # --------------- Image Detection Parameters for Edge Detection -------------- #
 EDGEDETECT = 3 # parameter for edge detect function. only change when needed.
-LEFTEDGEWINDOW = [[200, 450], [220, 260]] # window for left edge detect, specify X,Y ranges.
-#endregion
+LEFTEDGEWINDOW = [[200, 450], [120, 300]] # window for left edge detect, specify Y and X ranges respectively.
 
 
 # ------- Option to group and color label plots based on 'design infos' ------ #
@@ -124,7 +143,6 @@ PLOT_BY_COLUMN = False
 
 # Create output directory if it doesn't exist
 os.makedirs(OUTPUTPATH, exist_ok=True)
-
 
 # Check if the lengths of datasets and design_infos match
 if len(DATASETS) != len(DESIGN_INFOS):
@@ -184,7 +202,7 @@ for dataind in range(NUMDATA):
     data_crownprofiles[dataind] = [None] * len(laserIDrange)
     data_xcrownprofiles[dataind] = [None] * len(laserIDrange)
     data_crowns[dataind] = np.full((len(laserIDrange), 3), np.nan)  # Initialize with NaNs
-    angle_matrix[dataind] = np.full((len(laserIDrange), 3), np.nan)  # Initialize with NaNs
+    angle_matrix[dataind] = np.full((len(laserIDrange), 4), np.nan)  # Initialize with NaNs
 
 processedMessages = []
 #endregion Pre-Processing
@@ -220,99 +238,81 @@ for dataind, dataset in enumerate(DATASETS):
     #endregion Iterative Loop
     
     # ITERNATE FOR ALL LASERS PER DATASET
-    for laserIDind, (rowID, colID) in enumerate(zip(ROWRANGE, COLRANGE)):
-        print(f"Row {rowID} Column {colID}")
-        opdfilename = opdfilenameformat.format(rowID, colID)
-        filename = os.path.join(cubePath, f"{opdfilename}.fc.opd")
-        
-        # surface = Surface.load(filename_debug)
-        # surface.show()
+    # Iterate over all lasers per dataset using nested loops for rows and columns
+    for rowID in ROWRANGE:
+        for colID in COLRANGE:
+            laserIDind = (rowID - 1) * COLS + (colID - 1)
+            print(f"Processing Laser {laserIDind + 1}: Row {rowID}, Column {colID}")
             
-        # ---------------------------------------------------------------------------- #
-        #                              Reading .opd Files                              #
-        # ---------------------------------------------------------------------------- #
-        #region File Parsing
-        blocks, params, image_raw = read_wyko_opd(filename)  # Read the .opd file
-        image_raw = np.transpose(image_raw)
-        # Calculate Resolution
-        Resolution = float(params['Pixel_size']) * 1000  # um
-        
-        # print(params)  # Display all metadata
-        # print(len(image_raw))        
-        
-       
-        #endregion File Parsing
-            
-        # ---------------------------------------------------------------------------- #
-        #                               Image Processing                               #
-        # ---------------------------------------------------------------------------- #
-        #region Image Processing
-            
-        # ------------------------------ Edge Detection ------------------------------ #
-        laser_edge, image_raw_positive = edge_detection(image_raw, EDGEDETECT)
-        
-        
-        # ----------------------------- Laser Orientation ---------------------------- #  
-        try:  
-            leftedge_angle, center_CS = estimate_rotation_and_cs(laser_edge, Resolution, LEFTEDGEWINDOW, image_raw)
-        except Exception as e:
-            # Print the error message to the screen
-            print("An error occurred:", e)
-            traceback.print_exc()
-            
-            plt.figure(1)
-            plt.clf()
-            plt.imshow(laser_edge, cmap='jet', aspect='equal')
-            plt.colorbar(label='Z$(\\mu m)$')
-            plt.xlabel('Column Pixel')
-            plt.ylabel('Row Pixel')
-            plt.title('Raw Data', fontsize=13, color='b')
-        # print(f"Left Edge Angle: {leftedge_angle}")
-        # print(f"Center Coordinate System: {center_CS}")
-        
-        
-        # ------------------------------- Plane Fitting ------------------------------ #
-        data_processed_laser, theta_z_real, theta_x_real, theta_y_real = flatten(image_raw_positive, Resolution, center_CS, leftedge_angle)
-        # print(f"Theta_x (roll): {theta_x_real}")
-        # print(f"Theta_y (pitch): {theta_y_real}")
-        #endregion Image Processing
-        
-        # Store raw and processed data
-        data_raw[dataind][laserIDind] = image_raw
-        data_processed[dataind][laserIDind] = data_processed_laser
-        angle_matrix[dataind][laserIDind] = [theta_z_real, theta_x_real, theta_y_real]
-        
-        
-        # ---------------------------------------------------------------------------- #
-        #                           Crown Profile Extraction                           #
-        # ---------------------------------------------------------------------------- #
-        #region Profile Extraction
-        crown_profile, xcrown_profile = extract_crown_profiles(data_processed_laser, Resolution)
-        
-        # Apply filters to the crown and xcrown profiles
-        if APPLY_DELTA_FILTER:
-            crown_profile = crown_delta_filter(crown_profile, DELTA_THRESHOLD)
-            xcrown_profile = crown_delta_filter(xcrown_profile, DELTA_THRESHOLD)
-        if APPLY_AVERAGE_FILTER:
-            crown_profile = crown_average_filter(crown_profile, window_size=WINDOW_SIZE_INPUT, threshold=ANOMALY_THRESHOLD)
-            xcrown_profile = crown_average_filter(xcrown_profile, window_size=WINDOW_SIZE_INPUT, threshold=ANOMALY_THRESHOLD)
+            # ---------------------------------------------------------------------------- #
+            #                              Reading .opd Files                              #
+            # ---------------------------------------------------------------------------- #
+            opdfilename = opdfilenameformat.format(rowID, colID)
+            filename = os.path.join(cubePath, f"{opdfilename}.fc.opd")
+                        
+            try:
+                # Reading .opd Files
+                blocks, params, image_raw = read_wyko_opd(filename)
+                last_valid_image_raw = image_raw
+                
+            except FileNotFoundError:
+                if last_valid_image_raw is not None:
+                    print(f"File missing: {filename}. Using the last valid image.")
+                    image_raw = last_valid_image_raw
+                else:
+                    print(f"File missing: {filename}. No valid image found to copy from.")
+                    continue
+                       
 
-        # Calculate crown_values and xcrown values
-        edgedistance = 0  # pixel numbers
-        crown_value = 0 - 0.5 * (crown_profile[edgedistance, 1] + crown_profile[-edgedistance - 1, 1])
+            Resolution = float(params['Pixel_size']) * 1000
+            
+            # ---------------------------------------------------------------------------- #
+            #                               Image Processing                               #
+            # ---------------------------------------------------------------------------- #
+            laser_edge, image_raw_positive = edge_detection(image_raw, EDGEDETECT)
+            
+            try:
+                leftedge_angle, center_CS = estimate_rotation_and_cs(laser_edge, Resolution, LEFTEDGEWINDOW, image_raw)
+            except Exception as e:
+                print("An error occurred:", e)
+                traceback.print_exc()
+                continue
+            
+            # Plane Fitting
+            data_processed_laser, theta_z_real, roll_angle, pitch_angle = flatten(image_raw_positive, Resolution, center_CS, leftedge_angle)
+            yaw_angle = -leftedge_angle
+            
+            # Store raw and processed data
+            data_raw[dataind][laserIDind] = image_raw
+            data_processed[dataind][laserIDind] = data_processed_laser
+            angle_matrix[dataind][laserIDind] = [theta_z_real, roll_angle, pitch_angle, yaw_angle]
+            
+            # ---------------------------------------------------------------------------- #
+            #                           Crown Profile Extraction                           #
+            # ---------------------------------------------------------------------------- #
+            crown_profile, xcrown_profile = extract_crown_profiles(data_processed_laser, Resolution)
+            
+            # Apply filters to the crown and xcrown profiles
+            if APPLY_DELTA_FILTER:
+                crown_profile = crown_delta_filter(crown_profile, DELTA_THRESHOLD)
+                xcrown_profile = crown_delta_filter(xcrown_profile, DELTA_THRESHOLD)
+            if APPLY_AVERAGE_FILTER:
+                crown_profile = crown_average_filter(crown_profile, window_size=WINDOW_SIZE_INPUT, threshold=ANOMALY_THRESHOLD)
+                xcrown_profile = crown_average_filter(xcrown_profile, window_size=WINDOW_SIZE_INPUT, threshold=ANOMALY_THRESHOLD)
+            
+            # Calculate crown_values and xcrown values
+            edgedistance = 0 # pixel numbers
+            crown_value = 0 - 0.5 * (crown_profile[edgedistance, 1] + crown_profile[-edgedistance - 1, 1])
+            xcrownP_value = 0 - xcrown_profile[edgedistance, 1]
+            xcrownN_value = 0 - xcrown_profile[-edgedistance - 1, 1]
+            
+            # Store crown profiles and values
+            data_crownprofiles[dataind][laserIDind] = crown_profile
+            data_xcrownprofiles[dataind][laserIDind] = xcrown_profile
+            data_crowns[dataind][laserIDind] = [crown_value, xcrownP_value, xcrownN_value]
+            # print(f"Assigned to data_crowns[{dataind}][{laserIDind}]: YCrown = {crown_value}, XCrownP = {xcrownP_value}, XCrownN = {xcrownN_value}")
+            
         
-        xcrownP_value = 0 - xcrown_profile[edgedistance, 1]
-        xcrownN_value = 0 - xcrown_profile[-edgedistance - 1, 1]
-        
-        # Store crown profiles and values
-        data_crownprofiles[dataind][laserIDind] = crown_profile
-        data_xcrownprofiles[dataind][laserIDind] = xcrown_profile
-        data_crowns[dataind][laserIDind] = [crown_value, xcrownP_value, xcrownN_value]
-        
-        # print(f"Assigned to data_crowns[{dataind}][{laserIDind}]: YCrown = {crown_value}, XCrownP = {xcrownP_value}, XCrownN = {xcrownN_value}")
-        
-    
-        #endregion Crown Profile Extraction
 
 # %%
 #-------------------------- Print summary messages -------------------------- #
@@ -330,6 +330,15 @@ for dataind, dataset in enumerate(DATASETS):
     for laserIDind, crown_values in enumerate(data_crowns[dataind]):
         ycrown, xcrownP, xcrownN = crown_values
         print(f"  Laser {laserIDind + 1}: YCrown = {ycrown:.2f} nm, XCrownP = {xcrownP:.2f} nm, XCrownN = {xcrownN:.2f} nm")
+
+# ------------------------------- Print angles ------------------------------- #
+print("\nAngles for each dataset:")
+for dataind, dataset in enumerate(DATASETS):
+    waferID, cubeID = dataset.split('_CUBE_')
+    print(f"\n{waferID} CUBE {cubeID}:")
+    for laserIDind, angles in enumerate(angle_matrix[dataind]):
+        theta_z_real, roll_angle, pitch_angle, yaw_angle = angles
+        print(f"  Laser {laserIDind + 1}: Roll = {roll_angle:.2f} degrees, Pitch = {pitch_angle:.2f} degrees, Yaw = {yaw_angle:.2f} degrees")
 
 # # -------------------------- Print crown profiles -------------------------- #
 # print("\nCrown profiles for each dataset:")
