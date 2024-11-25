8960381b85ffe627feb54e72f963fe2ef175948c
{Vadan Khan} <{vadankhan1@gmail.com}>
2024-11-25 12:27:45 +0000

    Individual Laser Plots + Profile Plot Upgrades

---
+++ vadan_topography_wyko/plotting_functions.py
@@ -0,0 +1,41 @@
+import numpy as np
+import seaborn as sns
+
+
+# Function to remove the offset (mean) from image_raw
+def remove_offset(image_raw):
+    mean_value = np.nanmean(image_raw)
+    image_raw_no_offset = image_raw - mean_value
+    return image_raw_no_offset
+
+
+# Function to generate unique colors using Seaborn's color palette
+def generate_unique_colors(num_colors, colour_set: str):
+    colors = sns.color_palette(colour_set, num_colors)
+    return colors
+
+
+def generate_laser_plotting_order(
+    laserIDranges, row_dynamic, col_dynamic, rows, cols, plot_by_column, dynamic_arrays
+):
+    laser_plotting_order = []
+    for dataind, laserIDrange in enumerate(laserIDranges):
+        order = []
+        if dynamic_arrays:
+            current_rows = row_dynamic[dataind]
+            current_cols = col_dynamic[dataind]
+        else:
+            current_rows = rows
+            current_cols = cols
+
+        if plot_by_column:
+            for row in range(current_rows):
+                for col in range(current_cols):
+                    laserID = col * current_rows + row
+                    if laserID < len(laserIDrange):
+                        order.append(laserID)
+        else:
+            order = list(range(len(laserIDrange)))
+
+        laser_plotting_order.append(order)  # Append the order list to laser_plotting_order
+    return laser_plotting_order
