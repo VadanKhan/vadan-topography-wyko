03746b82433351d414b98d5f194aaa448d22990b
{Vadan Khan} <{vadankhan1@gmail.com}>
2024-12-10 14:42:13 +0000

    Flipped Image Orientation to Match Past Conventions

--- vadan_topography_wyko/opd_read.py
+++ vadan_topography_wyko/opd_read.py
@@ -3,17 +3,18 @@ import struct
 import codecs
 import matplotlib.pyplot as plt
 
+
 def read_wyko_opd(filename):
     # print("meow")
     # Open the file in binary mode and read its content
-    with open(filename, 'rb') as fid:
+    with open(filename, "rb") as fid:
         E = fid.read()
 
     # Convert the content to the specified encoding
-    E2 = codecs.encode(E.decode('ISO-8859-1'), 'ISO-8859-1')
+    E2 = codecs.encode(E.decode("ISO-8859-1"), "ISO-8859-1")
 
     # Find the first instance of the word 'Directory'
-    ind = E2.find(b'Directory')
+    ind = E2.find(b"Directory")
     # print(ind)
 
     # Initialize variables
@@ -25,50 +26,50 @@ def read_wyko_opd(filename):
     # Parse the directory
     while ind + 21 <= Directorybytes:
         block = {
-            'name': E2[ind:ind+16].decode('ISO-8859-1'),
-            'type': struct.unpack('h', E2[ind+16:ind+18])[0],
-            'Length': struct.unpack('i', E2[ind+18:ind+22])[0],
-            'BlockTail': struct.unpack('h', E2[ind+22:ind+24])[0]
+            "name": E2[ind : ind + 16].decode("ISO-8859-1"),
+            "type": struct.unpack("h", E2[ind + 16 : ind + 18])[0],
+            "Length": struct.unpack("i", E2[ind + 18 : ind + 22])[0],
+            "BlockTail": struct.unpack("h", E2[ind + 22 : ind + 24])[0],
         }
         BLOCKS.append(block)
         BlockID += 1
         ind += Block_len
 
     # Extract parameters
-    Parameters = ['Pixel_size', 'Wavelength']
+    Parameters = ["Pixel_size", "Wavelength"]
     ParametersValue = {}
 
     for param in Parameters:
         for block in BLOCKS:
-            if block['name'].startswith(param):
+            if block["name"].startswith(param):
                 # Find the index of the current block
                 current_block_index = BLOCKS.index(block)
 
                 # Sum the lengths of all blocks before the current block
-                # NOTE THAT THIS ADDS 2, NOT 3. 
-                ind = 2 + sum(b['Length'] for b in BLOCKS[:current_block_index])
+                # NOTE THAT THIS ADDS 2, NOT 3.
+                ind = 2 + sum(b["Length"] for b in BLOCKS[:current_block_index])
 
                 # Extract the raw bytes
-                raw_bytes = E2[ind:ind+block['Length']]
-                ParametersValue[param] = struct.unpack('f', raw_bytes)[0]
-                
+                raw_bytes = E2[ind : ind + block["Length"]]
+                ParametersValue[param] = struct.unpack("f", raw_bytes)[0]
+
                 # Print the block, raw bytes, and block index for debugging
                 # print(f"Block for {param}: {block}")
                 # print(f"Raw bytes for {param}: {list(raw_bytes)}")
                 # print(f"Block index for {param}: {current_block_index}")
                 break
-    
+
     # Read RAW_DATA
     # Calculate the starting index for reading raw data by summing the lengths of the first three blocks
-    ind = 2 + BLOCKS[0]['Length'] + BLOCKS[1]['Length'] + BLOCKS[2]['Length']
+    ind = 2 + BLOCKS[0]["Length"] + BLOCKS[1]["Length"] + BLOCKS[2]["Length"]
     # print(ind)
 
     # Read the X and Y dimensions of the data
-    Xsize = int(struct.unpack('h', E2[ind:ind+2])[0])
-    Ysize = int(struct.unpack('h', E2[ind+2:ind+4])[0])
+    Xsize = int(struct.unpack("h", E2[ind : ind + 2])[0])
+    Ysize = int(struct.unpack("h", E2[ind + 2 : ind + 4])[0])
 
     # Read the number of bytes per data point
-    Nbytes_data = int(struct.unpack('h', E2[ind+4:ind+6])[0])
+    Nbytes_data = int(struct.unpack("h", E2[ind + 4 : ind + 6])[0])
     # print(Nbytes_data)
     pixel_bytes = Nbytes_data
 
@@ -80,18 +81,27 @@ def read_wyko_opd(filename):
 
     # Read each pixel's data
     for pid in range(Xsize * Ysize):
-        pixeldata[pid] = struct.unpack('f', E2[ind+(pid*pixel_bytes):ind+(pid*pixel_bytes)+pixel_bytes])[0]
+        pixeldata[pid] = struct.unpack(
+            "f", E2[ind + (pid * pixel_bytes) : ind + (pid * pixel_bytes) + pixel_bytes]
+        )[0]
 
     # Find and replace invalid pixel values with NaN
     idx = np.where(pixeldata >= 1e10)
     pixeldata[idx] = np.nan
 
     # Calculate the raw image data using the wavelength parameter
-    VSIWavelength = ParametersValue['Wavelength']
+    VSIWavelength = ParametersValue["Wavelength"]
     image_raw = np.reshape(pixeldata, (Xsize, Ysize)) * VSIWavelength
-    - np.nanmean(pixeldata) * VSIWavelength # NOTE THE ORDER OF XSIZE AND YSIZE, differs between matlab and python
-    
-    image_raw = np.transpose(image_raw)
+    -np.nanmean(
+        pixeldata
+    ) * VSIWavelength  # NOTE THE ORDER OF XSIZE AND YSIZE, differs between matlab and python
+
+    # Step 1 to getting image in desired format (vertically aligned)
+    image_raw_flipped = np.transpose(image_raw)
+
+    # Step 1 to getting image in desired format (agreeing with orientation in previous code
+    # and previous conventions for angles)
+    image_raw = np.flipud(image_raw_flipped)
 
     # # Debug plot of image_raw
     # plt.figure()
